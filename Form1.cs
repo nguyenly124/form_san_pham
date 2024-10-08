ca5aa1c44a0cdb54185a8dee62e6cbbdefc282e5
nguyenly124 <nguyenthily124da@gmail.com>
2024-10-08 14:38:50 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace form_san_pham
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
